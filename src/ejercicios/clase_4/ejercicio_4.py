0c245d694e4048645a1474405103fc199b1ec36f
diegogocru617-tech <diegogocru617@gmail.com>
2025-08-22 09:42:04 -0600

    Update ejercicio_4.py
    Diego Gomez Cruz A01826315

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,11 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+num = int(input("Ingrese un número: "))
+if num < 0:
+    print("Negativo")
+elif num == 0:
+    print("Cero")
+else:
+    num > 0
+    print("Positivo")
